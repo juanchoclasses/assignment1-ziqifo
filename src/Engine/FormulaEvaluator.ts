b9b9ce5cccd654b9b7b73403fd13bfcf6ff65f66
ziqifo <fo.z@northeastern.edu>
2023-10-02 03:23:32 -0700

    Update Evaluator

--- src/Engine/FormulaEvaluator.ts
+++ src/Engine/FormulaEvaluator.ts
@@ -43,14 +43,135 @@ export class FormulaEvaluator {
     * 
    */
 
-  evaluate(formula: FormulaType) {
+evaluate(formula: FormulaType) {
+  // Initialize error message and result
+  this._initializeEvaluation();
 
+  // Check for empty formula
+  if (this._isEmptyFormula(formula)) return;
 
-    // set the this._result to the length of the formula
+  // Replace cell references with their values in the formula
+  const formulaInValue = this._replaceCellReferencesWithValues(formula);
+  if (this._errorMessage !== "") return;
 
-    this._result = formula.length;
-    this._errorMessage = "";
+  // Convert infix expression to postfix
+  const outputQueue = this._convertToPostfix(formulaInValue);
+  if (this._errorMessage !== "") return;
+
+  // If the formula is a single number, return that number
+  if (this._isSingleNumber(outputQueue)) return;
+
+  // Evaluate the postfix expression
+  this._evaluatePostfix(outputQueue);
+}
+
+private _initializeEvaluation() {
+  this._errorMessage = "";
+  this._result = 0;
+}
+
+private _isEmptyFormula(formula: FormulaType): boolean {
+  if (formula.length === 0) {
+    this._errorMessage = ErrorMessages.emptyFormula;
+    return true;
   }
+  return false;
+}
+
+private _replaceCellReferencesWithValues(formula: FormulaType): string[] {
+  const formulaInValue: string[] = [...formula];
+  formula.forEach((token, i) => {
+    if (this.isCellReference(token)) {
+      const [value, error] = this.getCellValue(token);
+      this._errorMessage = error;
+      formulaInValue[i] = String(value);
+    }
+  });
+  return formulaInValue;
+}
+
+private _convertToPostfix(formulaInValue: string[]): TokenType[] {
+  const operatorStack: TokenType[] = [];
+  const outputQueue: TokenType[] = [];
+  const precedence: { [key: string]: number } = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2
+  };
+
+  for (const token of formulaInValue) {
+    if (this.isNumber(token)) {
+      outputQueue.push(token);
+    } else if (token === '(') {
+      operatorStack.push(token);
+    } else if (token === ')') {
+      while (operatorStack.length && operatorStack[operatorStack.length - 1] !== '(') {
+        outputQueue.push(operatorStack.pop()!);
+      }
+      operatorStack.pop();
+    } else {
+      while (operatorStack.length && precedence[token] <= precedence[operatorStack[operatorStack.length - 1]]) {
+        outputQueue.push(operatorStack.pop()!);
+      }
+      operatorStack.push(token);
+    }
+  }
+
+  while (operatorStack.length) {
+    outputQueue.push(operatorStack.pop()!);
+  }
+
+  return outputQueue;
+}
+
+private _isSingleNumber(outputQueue: TokenType[]): boolean {
+  if (outputQueue.length === 1 && this.isNumber(outputQueue[0])) {
+    this._result = Number(outputQueue[0]);
+    return true;
+  }
+  return false;
+}
+
+private _evaluatePostfix(outputQueue: TokenType[]) {
+  const valueStack: number[] = [];
+  for (const token of outputQueue) {
+    if (this.isNumber(token)) {
+      valueStack.push(Number(token));
+    } else {
+      const b = valueStack.pop();
+      const a = valueStack.pop();
+      if (b === undefined || a === undefined) {
+        this._errorMessage = ErrorMessages.invalidFormula;
+        return;
+      }
+      switch (token) {
+        case '+':
+          valueStack.push(a + b);
+          break;
+        case '-':
+          valueStack.push(a - b);
+          break;
+        case '*':
+          valueStack.push(a * b);
+          break;
+        case '/':
+          if (b === 0) {
+            this._errorMessage = ErrorMessages.divideByZero;
+            this._result = Infinity;
+            return;
+          } else {
+            valueStack.push(a / b);
+          }
+          break;
+        default:
+          this._errorMessage = ErrorMessages.invalidOperator;
+          return;
+      }
+    }
+  }
+  this._result = valueStack[0];
+}
 
   public get error(): string {
     return this._errorMessage
